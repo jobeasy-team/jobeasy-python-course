e5962031917634241afbf46e37a7d7f5e8ec76f4
bizwebmaster <temur@danelia.info>
2020-04-13 12:40:11 -0700

    Done

--- lesson_1/homework_1_2.py
+++ lesson_1/homework_1_2.py
@@ -1,19 +1,19 @@
 # Find the second power of a variable. Save the expression to result_1 variable
 
 a = 10
-result_1 = None
+result_1 = a ** 2
 
 
 # Convert integer variable b to a float. Save the expression to result_2 variable
 
 b = 10
-result_2 = None
+result_2 = float(b)
 
 
 # Convert a float variable c to integer. Save the expression to result_3 variable
 
 c = 5.04
-result_3 = None
+result_3 = int(c)
 
 
 # Sum up variables d and e and then multiply the total by f. Convert result to an integer and save the
@@ -22,4 +22,4 @@ result_3 = None
 d = 5.04
 e = 2
 f = 1.2
-result_4 = None
+result_4 = int((d + e) * f)
--- lesson_1/homework_1_3.py
+++ lesson_1/homework_1_3.py
@@ -4,15 +4,16 @@ import math
 # Round a off to three decimal places.
 
 a = 10.04
-result_1 = None
+result_1 = round(a, 3)
 
 
 # Round up result_2 to greater value.
 
 result_2 = 5 / 2 * 6 + 1.25 - 4
-
+math.ceil(result_2)
 
 # Round up result_3 to lesser value.
 
 result_3 = 8 / 3 * 5 + 4.75 - 7
+math.floor(result_3)
 
