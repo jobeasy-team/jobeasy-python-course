5499499fc328f3319c88527658ef5f53ad7e4ba9
lion7500000 <lion7500000@gmail.com>
2020-04-15 16:21:05 -0700

    Home Work 1

--- tests/homework_1_1.py
+++ tests/homework_1_1.py
@@ -101,14 +101,14 @@ def test_division():
 def test_division_floored():
     # Do floor division for i and j. Save the expression to the result_division_floored variable
     if not result_division_floored:
-        pytest.skip(f"You didn't finish this task. the result variable equals None")
+        pytest.skip (f"You didn't finish this task. the result variable equals None")
     assert i // j == result_division_floored, f'Expected {i // j}, but got {result_division_floored}'
 
 
 def test_division_remainder():
     # Calculate the remainder of k divided by l. Save expression to the result_division_remainder variable
     if not result_division_remainder:
-        pytest.skip(f"You didn't finish this task. the result variable equals None")
+        pytest.skip (f"You didn't finish this task. the result variable equals None")
     assert k % l == result_division_remainder, f'Expected {k % l}, but got {result_division_remainder}'
 
 
