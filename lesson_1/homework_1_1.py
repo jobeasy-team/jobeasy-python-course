6d6e5979d9b748ed214007f6c7fa75189fa8c714
oksusenko <63606531+oksusenko@users.noreply.github.com>
2020-04-14 21:50:49 -0500

    Homework First Lesson

--- lesson_1/homework_1_1.py
+++ lesson_1/homework_1_1.py
@@ -2,52 +2,51 @@
 
 a = 10
 b = 15
-result_sum = None
+result_sum = a + b
 
 
 # Find the difference between c and d. Save the expression to the result_diff variable
 
 c = 10
 d = 15
-result_diff = None
+result_diff = c - d
 
 
 # Multiply e by f. Save the expression to the result_multiplication variable
 
 e = 10
 f = 15
-result_multiplication = None
+result_multiplication = e * f
 
 
 # Divide g by h. Save the expression to the result_division variable
 
 g = 10
 h = 15
-result_division = None
+result_division = g / h
 
 
 # Calculate the floored division result of i and j. Save the expression to the result_division_floored variable
 
 i = 10
 j = 15
-result_division_floored = None
+result_division_floored = i // j
 
 
 # Calculate the remainder of k divided by l. Save expression to the result_division_remainder variable
 
 k = 10
 l = 15
-result_division_remainder = None
+result_division_remainder = k % l
 
 
 # Rise m to the power n. Save the expression to the result_power variable
 
 m = 5
 n = 2
-result_power = None
-
+result_power = m ** n
 
 # Convert an o variable to negative. Save the expression to the result_negative variable
 
 o = 100
-result_negative = None
+result_negative = - o
