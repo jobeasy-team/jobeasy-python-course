3eb79d4e84444ea26a33c732877af2549c98bf43
Ilya Tishin <nenaviju4tverg@gmail.com>
2020-04-16 12:52:29 -0700

    add homework 2 and homework 3

---
+++ lesson_2/homework_2_3.py
@@ -0,0 +1,6 @@
+# Write your first program. Input the temperature right now in Fahrenheit and convert it to Celsius.
+# !important you should save only number to result_temperature. Formula (32°F − 32) × 5/9 = 0°C
+
+# type your code here
+temperature_fahrenheit = None
+result_temperature = None
---
+++ tests/homework_2_3.py
@@ -0,0 +1,73 @@
+# PLEASE, DON'T CHEAT
+# PLEASE, DON'T CHEAT
+# PLEASE, DON'T CHEAT
+# PLEASE, DON'T CHEAT
+# PLEASE, DON'T CHEAT
+# PLEASE, DON'T CHEAT
+# PLEASE, DON'T CHEAT
+# PLEASE, DON'T CHEAT
+# PLEASE, DON'T CHEAT
+# PLEASE, DON'T CHEAT
+# PLEASE, DON'T CHEAT
+# PLEASE, DON'T CHEAT
+# PLEASE, DON'T CHEAT
+# PLEASE, DON'T CHEAT
+# PLEASE, DON'T CHEAT
+# PLEASE, DON'T CHEAT
+# PLEASE, DON'T CHEAT
+# PLEASE, DON'T CHEAT
+# PLEASE, DON'T CHEAT
+# PLEASE, DON'T CHEAT
+# PLEASE, DON'T CHEAT
+# PLEASE, DON'T CHEAT
+# PLEASE, DON'T CHEAT
+# PLEASE, DON'T CHEAT
+# PLEASE, DON'T CHEAT
+# PLEASE, DON'T CHEAT
+# PLEASE, DON'T CHEAT
+# PLEASE, DON'T CHEAT
+# PLEASE, DON'T CHEAT
+# PLEASE, DON'T CHEAT
+# PLEASE, DON'T CHEAT
+# PLEASE, DON'T CHEAT
+# PLEASE, DON'T CHEAT
+# PLEASE, DON'T CHEAT
+# PLEASE, DON'T CHEAT
+# PLEASE, DON'T CHEAT
+# PLEASE, DON'T CHEAT
+# PLEASE, DON'T CHEAT
+# PLEASE, DON'T CHEAT
+# PLEASE, DON'T CHEAT
+# PLEASE, DON'T CHEAT
+# PLEASE, DON'T CHEAT
+# PLEASE, DON'T CHEAT
+# PLEASE, DON'T CHEAT
+# PLEASE, DON'T CHEAT
+# PLEASE, DON'T CHEAT
+# PLEASE, DON'T CHEAT
+# PLEASE, DON'T CHEAT
+# PLEASE, DON'T CHEAT
+# PLEASE, DON'T CHEAT
+# PLEASE, DON'T CHEAT
+# PLEASE, DON'T CHEAT
+# PLEASE, DON'T CHEAT
+# PLEASE, DON'T CHEAT
+# PLEASE, DON'T CHEAT
+
+
+# THIS FILE HAS ANSWERS. USE IT ONLY WHEN YOU HAVE SUCCESSFULLY PASSED HOMEWORK TESTS
+
+
+from lesson_2.homework_2_3 import temperature_fahrenheit, result_temperature
+import pytest
+
+
+def test_fahrenheit():
+    # Write your first program. Input the temperature right now in Fahrenheit and convert it to Celsius.
+    # !important you should save only number to result_temperature. Formula (32°F − 32) × 5/9 = 0°C
+    if temperature_fahrenheit == None or result_temperature == None:
+        pytest.skip(f"You didn't finish this task. the result variable equals None")
+    assert (temperature_fahrenheit - 32) * 5 / 9 == result_temperature
+
+
+
