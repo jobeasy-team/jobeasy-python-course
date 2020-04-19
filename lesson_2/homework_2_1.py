0cffbb13f45d2f16808d0d4bac532ec6f3cb3d6f
oksusenko <63606531+oksusenko@users.noreply.github.com>
2020-04-18 22:57:48 -0500

    Second lesson homework

--- lesson_2/homework_2_1.py
+++ lesson_2/homework_2_1.py
@@ -1,33 +1,36 @@
 # Create three strings using three different methods. Save your result to result_string_1, result_string_2,
 # result_string_3 variables
 
-result_string_1 = None
-result_string_2 = None
-result_string_3 = None
+result_string_1 = "This is my first string"
+result_string_2 = "5 is my favorite number"
+result_string_3 = "P0000504"
 
 
 # Enter a string with your name using console and save the result to result_name variable
 
-result_name = None
+result_name = "Oksana"
+print(result_name)
 
 
 # Enter your first and  last name. Join them together with a space in
 # between. Save a result in a variable result_full_name and
 # save the length of the whole name in result_full_name_length variable.
 
-first_name = None
-last_name = None
-result_full_name = None
-result_full_name_length = None
+first_name = "Oksana"
+last_name = "Usenko"
+result_full_name = first_name + " " + last_name
+result_full_name_length = len(result_full_name)
 
 
 # Enter the capital city of California State in lower case. Change the case to title case.
 # Save the result in result_ca_capital variable
 
-result_ca_capital = None
+result_ca_capital = "sacramento"
+print(result_ca_capital.lower())
 
 
 # Enter the name of our planet. Change the case to upper case. Save the result in
 # result_planet variable
 
-result_planet = None
+result_planet = "EARTH"
+print(result_planet.capitalize())
