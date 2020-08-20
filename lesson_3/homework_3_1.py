d1d38c50246ad5aca1dc63dd402d78ad7b56ec27
Ilya Tishin <nenaviju4tverg@gmail.com>
2020-08-20 16:19:35 -0700

    add homework 3

---
+++ lesson_3/homework_3_1.py
@@ -0,0 +1,26 @@
+# FOR LOOPS EXERCISES
+
+# Enter your name, save it in name variable and save in result_1 variable your name repeated 3 times (use loops)
+
+name_1 = None
+result_1 = None
+
+
+# Modify your previous program so that it will enter your name (save it in variable  name_2) and a number
+# (save in variable number) and then save in result_2 variable your name repeated as many times as number_1 is
+# (use loops)
+name_2 = None
+number_1 = None
+result_2 = None
+
+
+# Enter a random string, which includes only digits. Write code which find a sum of digits in this string and save it
+# into result_3 variable
+
+string_number_1 = None
+result_3 = None
+
+
+# Create code which sums up all even numbers between 2 and 100 (include 100) and save it in result_4 variable
+
+result_4 = None
