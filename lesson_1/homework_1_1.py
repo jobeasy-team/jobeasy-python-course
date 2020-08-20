43830bfcf9b47282def3425cc56fc0c852ad274e
Valli <rmvkdeals1@gmail.com>
2020-08-20 08:40:25 -0400

    Updated lesson1 homework 1_1

--- lesson_1/homework_1_1.py
+++ lesson_1/homework_1_1.py
@@ -2,52 +2,68 @@
 
 a = 10
 b = 15
-result_sum = None
+result_sum = a+b
+print ('sum')
+print(result_sum)
 
 
 # Find the difference between c and d. Save the expression to the result_diff variable
 
 c = 10
 d = 15
-result_diff = None
+print ('diff')
+result_diff = c - d
+print (result_diff)
 
 
 # Multiply e by f. Save the expression to the result_multiplication variable
 
 e = 10
 f = 15
-result_multiplication = None
+result_multiplication = e * f
+print("multiplication")
+print(result_multiplication)
 
 
 # Divide g by h. Save the expression to the result_division variable
 
 g = 10
 h = 15
-result_division = None
+result_division = g/h
+print("division")
+print(result_division)
 
 
 # Calculate the floored division result of i and j. Save the expression to the result_division_floored variable
 
 i = 10
 j = 15
-result_division_floored = None
+result_division_floored = i // j
+print("floored division")
+print(result_division_floored)
 
 
 # Calculate the remainder of k divided by l. Save expression to the result_division_remainder variable
 
 k = 10
 l = 15
-result_division_remainder = None
+result_division_remainder = k % l
+print("remainder division")
+print(result_division_remainder)
 
 
 # Rise m to the power n. Save the expression to the result_power variable
 
 m = 5
 n = 2
-result_power = None
+result_power = m ** n
+print("power variable")
+print(result_power)
 
 
 # Convert an o variable to negative. Save the expression to the result_negative variable
 
 o = 100
-result_negative = None
+result_negative = -1 * o
+print("negative variable")
+print(result_negative)
