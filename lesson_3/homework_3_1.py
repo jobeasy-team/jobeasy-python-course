7d4d6ebf33f426eb23da6175451977fca84e9491
ratna-tech <ratnasinha012@gmail.com>
2020-08-21 16:28:20 -0400

    Submitting homework 3

--- lesson_3/homework_3_1.py
+++ lesson_3/homework_3_1.py
@@ -3,8 +3,11 @@
 # Ex. 1
 # Enter your name, save it in name variable and save in result_1 variable your name repeated 3 times (use loops)
 
-name_1 = None
-result_1 = None
+#name_1 = "Ratna"
+#result_1 = None
+#for i in range(3):
+ #   result_1 = name_1
+  #  print(result_1)
 
 # TODO: Here is your code
 
@@ -13,10 +16,12 @@ result_1 = None
 # Modify your previous program so that it will enter your name (save it in variable  name_2) and a number
 # (save in variable number) and then save in result_2 variable your name repeated as many times as number_1 is
 # (use loops)
-name_2 = None
-number_1 = None
+name_2 = "Ratna"
+number_1 = 4
 result_2 = None
-
+for i in range(number_1):
+    result_2 = name_2
+    print(result_2)
 # TODO: Here is your code
 
 
@@ -24,15 +29,19 @@ result_2 = None
 # Enter a random string, which includes only digits. Write code which find a sum of digits in this string and save it
 # into result_3 variable
 
-string_number_1 = None
-result_3 = None
-
+string_number_1 = "23456"
+result_3 = 0
+for letter in string_number_1:
+    result_3 += int(letter)
+print(result_3)
 # TODO: Here is your code
 
 
 # Ex. 4
 # Create code which sums up all even numbers between 2 and 100 (include 100) and save it in result_4 variable
 
-result_4 = None
-
+result_4 = 0
+for i in range(2,102,2):
+    result_4 = (result_4 + i)
+print(result_4)
 # TODO: Here is your code
--- lesson_3/homework_3_2.py
+++ lesson_3/homework_3_2.py
@@ -4,20 +4,25 @@
 # Write code, which will count how many times your character is included in your string.
 # Save result to result_1 variable
 
-string_1 = None
-char_1 = None
-result_1 = None
+string_1 = 'It is a random string in a variable'
+char_1 = 'a'
+result_1 = string_1.count(char_1)
+print(result_1)
 
 
 # Enter a random number and save it in variable number_1. Then create code to multiply all the digits together
 # and save result in the result_2 variable.
+result_2 = 1
+number_1 = "2345"
+for i in number_1:
+    result_2 = result_2 * int(i)
+print(result_2)
 
-number_1 = None
-result_2 = None
 
 
 # Enter a random number and save it in variable number_2. Then create code which will return
 # a number with digits of number_1 in reverse order. Save it in result_3 variable
 
-number_2 = None
-result_3 = None
+number_2 = "1234"
+result_3 = number_2[::-1]
+print(result_3)
