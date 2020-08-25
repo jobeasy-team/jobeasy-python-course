e54491f19404aa23500362723cbc3cc39e1d1921
Ilya Tishin <nenaviju4tverg@gmail.com>
2020-08-25 16:48:36 -0700

    add homework 5

---
+++ lesson_5/homework_5_1.py
@@ -0,0 +1,40 @@
+# FUNCTIONS
+
+# Difference
+# Write a function, which will calculate the difference of these two numbers
+
+def difference(num_1, num_2):
+    pass
+
+
+# Division
+# Write a function, which will divide these two numbers
+
+def division(num_1, num_2):
+    pass
+
+
+# Function gets random number. If this number is more than ten, return the difference between 100 and this number,
+# otherwise return this number multiplied by 10
+
+def function_1(number):
+    pass
+
+
+# Your function temerature_convertor gets the temperature in Fahrenheit, convert it to Celsius and return.
+# Formula (32°F − 32) × 5/9 = 0°C
+
+def temerature_convertor(fahrenheit_degree):
+    pass
+
+
+# Taxi Fare
+# In a particular jurisdiction, taxi fares consist of a base fare of $4.00, plus $0.25 for every 140 meters travelled.
+# Write a function that takes the distance travelled (in kilometers) as its only parameter and returns the total fare
+# as its only result rounded by 2 digits. Write a program that demonstrates the function.
+
+def taxi_fare(distance):
+    pass
+
+# examples of usage:
+# taxi_fare(10) #21.86
