7d7218e148c7a09ce9e810325805c8c76607ed0e
pecholi <bikmansquad@gmail.com>
2020-08-25 12:54:42 -0500

    hw 4 corrected based on pytest

--- lesson_1/homework_1_4.py
+++ lesson_1/homework_1_4.py
@@ -1,28 +1,30 @@
 # Create three strings using three different methods. Save your result to result_string_1, result_string_2,
 # result_string_3 variables
 
-result_string_1 = None
-result_string_2 = None
-result_string_3 = None
+result_string_1 = 'a'
+result_string_2 = "b"
+result_string_3 = result_string_1 + result_string_2
 
 
 # Enter your first and  last name. Join them together with a space in
 # between. Save a result in a variable result_full_name and
 # save the length of the whole name in result_full_name_length variable.
 
-first_name = None
-last_name = None
-result_full_name = None
-result_full_name_length = None
+first_name = 'James'
+last_name = 'Bond'
+result_full_name = first_name + ' ' + last_name
+result_full_name_length = len(result_full_name)
 
 
 # Enter the capital city of California State in lower case. Change the case to title case.
 # Save the result in result_ca_capital variable
 
-result_ca_capital = None
+ca_capital_city = 'sacramento'
+result_ca_capital = ca_capital_city.capitalize()
 
 
 # Enter the name of our planet. Change the case to upper case. Save the result in
 # result_planet variable
 
-result_planet = None
+planet = 'earth'
+result_planet = planet.upper()
