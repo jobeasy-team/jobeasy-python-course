778775a39ca99d12cdb80f338251edcee5b826fe
pecholi <bikmansquad@gmail.com>
2020-08-25 12:52:01 -0500

    hw 3 corrected based on pytest

--- lesson_1/homework_1_3.py
+++ lesson_1/homework_1_3.py
@@ -9,9 +9,9 @@ result_1 = round(a, 3)
 # Round up result_2 to greater value.
 
 result_2 = 5 / 2 * 6 + 1.25 - 4
-math.ceil(result_2)
+result_2 = math.ceil(result_2)
 
 # Round up result_3 to lesser value.
 
 result_3 = 8 / 3 * 5 + 4.75 - 7
-math.floor(result_3)
+result_3 = math.floor(result_3)
