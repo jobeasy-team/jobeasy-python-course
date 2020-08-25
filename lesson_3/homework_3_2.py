e3f0ff0e93124c985063a7e1d231bc0df33fa11b
ratna-tech <ratnasinha012@gmail.com>
2020-08-25 15:49:07 -0400

    resubmitting homework_3_2

--- lesson_3/homework_3_2.py
+++ lesson_3/homework_3_2.py
@@ -13,9 +13,11 @@ print(result_1)
 # Enter a random number and save it in variable number_1. Then create code to multiply all the digits together
 # and save result in the result_2 variable.
 result_2 = 1
-number_1 = "2345"
-for i in number_1:
-    result_2 = result_2 * int(i)
+number_1 = int("2345")
+while number_1 > 0:
+    rem = number_1 % 10
+    result_2 = result_2 * rem
+    number_1 = int(number_1/10)
 print(result_2)
 
 
@@ -23,6 +25,11 @@ print(result_2)
 # Enter a random number and save it in variable number_2. Then create code which will return
 # a number with digits of number_1 in reverse order. Save it in result_3 variable
 
-number_2 = "1234"
-result_3 = number_2[::-1]
+number_2 = int("1234")
+#result_3 = number_2[::-1]
+result_3 = ''
+while number_2 >0:
+    rem = number_2 % 10
+    result_3 = result_3 + str(rem)
+    number_2 = int(number_2/10)
 print(result_3)
