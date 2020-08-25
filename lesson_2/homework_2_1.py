ec7ffe682c87a9b4590285eec129a91722021687
Valli <rmvkdeals1@gmail.com>
2020-08-24 22:36:58 -0400

    homework 2

--- lesson_2/homework_2_1.py
+++ lesson_2/homework_2_1.py
@@ -1,31 +1,65 @@
 # Enter two numbers. If the first one is greater than the second, save first number in result_1,
 # otherwise save the second number to the result_1 variable.
 
-first_number = None
-second_number = None
-result_1 = None
+#first_number = None
+#second_number = None
+#result_1 = None
+
+
+first_number = 40
+second_number = 200
+if first_number > second_number:
+ result_1 = first_number
+else:
+ result_1 = second_number
+ print('result_1 '+ str(result_1))
 
 # Enter a random number in number_1 variable. If this number is 20 or
 # higher save “Too high” text to result_2, otherwise save “Thank you”.
-number_1 = None
-result_2 = None
+#number_1 = None
+#result_2 = None
+
+number_1 = 10
+if number_1 >= 20:
+    result_2 = "Too high"
+else:
+    result_2 = "Thank you"
+print('result_2 '+ result_2)
 
 
 # Enter your first name and last name in first_name and last_name variables. If the length of your first name is under
 # five characters, join them together (without a space) and save it to result_3 variable in upper case. If the length
 # of the first name is five or more characters, save their first name in lower case in result_3 variable.
 
-first_name = None
-last_name = None
-result_3 = None
+#first_name = None
+#last_name = None
+#result_3 = None
+
+first_name = 'valli'
+last_name = 'ram'
+if len(first_name) < 5:
+    result_3 = first_name.upper() + last_name.upper()
+else:
+    result_3 = first_name.lower()
+print("result_3 " + result_3)
+
+
 
 
 # Enter a number between 10 and 20 (inclusive) and save number to number_2 variable
 # If they enter a number within this range, save a message “Thank you” to result_4, otherwise a
 # message “Incorrect answer” to result_4.
 
-number_2 = None
-result_4 = None
+#number_2 = None
+#result_4 = None
+
+number_2 = 20
+if number_2 in range(10, 21):
+   result_4 = "Thank you"
+else:
+   result_4 = "Incorrect answer"
+print("result_4 " + result_4)
+
 
 
 # Enter your age. If you are 18 or over, save the message “You can vote” in result_5,
@@ -33,13 +67,32 @@ result_4 = None
 # if you are 16, save the message “You can buy a lottery ticket” in result_5,
 # if you are under 16, save the message “You can go Trick-or-Treating” in result_5 variable.
 
-age = None
-result_5 = None
+#age = None
+#result_5 = None
+
+age = 18
+if age >= 18:
+    result_5 = "You can vote"
+elif age == 17:
+    result_5 = "You can learn to drive"
+elif age == 16:
+    result_5 = "you can buy a lottery ticket"
+else:
+    result_5 = "you can go Trick-or-Treating"
+print("result_5 " + result_5)
 
 
 # Enter a number between 1 and 12, save this value to month variable. Find which month is it.
 # (January, February, March, April, May, June, Jule, August, September, October, November, December)
 # Write answer in result_month in lower case
 
-month = None
-result_month = None
+#month = None
+#result_month = None
+
+import calendar
+month = 5
+if month in range(1,13):
+    result_month = calendar.month_name[month].lower()
+else:
+    result_month = "invalid number"
+print("result_month " + result_month)
--- lesson_2/homework_2_3.py
+++ lesson_2/homework_2_3.py
@@ -1,21 +1,36 @@
 # Save to variable result_1 the first character of string_1 variable. In result_2 save the last character
 # of string_1. Use indexes.
 
+#string_1 = 'Python'
+#result_1 = None
+#result_2 = None
+
 string_1 = 'Python'
-result_1 = None
-result_2 = None
+result_1 = string_1[0]
+print('first character ' + result_1)
+result_2 =  string_1[-1]
+print('last character ' + result_2)
 
 
 # Save to variable result_3 string value from string_2 variable, written in reverse order, using concatenation.
 
+#string_2 = 'Python'
+#result_3 = None
+
 string_2 = 'Python'
-result_3 = None
+print(len(string_2))
+result_3 = string_2[5] + string_2[4] + string_2[3] + string_2[2] + string_2[1] + string_2[0]
+print(result_3)
+#for i in range(0,n,1):
 
 
 # Slice string string_3 from 5th to 20th (excluding 20th) character and save the result to variable result_4
 
 string_3 = 'Python is a programming language that lets you work quickly and integrate systems more effectively'
-result_4 = None
+#result_4 = None
+result_4 = string_3[5:20]
+print("result_4 " + result_4)
+
 
 
 # Slice string string_4 from 10th character to the end of the string. Save only every second character to variable
@@ -23,6 +38,8 @@ result_4 = None
 
 string_4 = 'Python is a programming language that lets you work quickly and integrate systems more effectively'
 result_5 = None
+result_5 = string_4[10::2]
+print("result_5 " + result_5)
 
 
 # Slice string string_5 from the first to the last character, save only every forth character and
@@ -30,6 +47,8 @@ result_5 = None
 
 string_5 = 'Python is a programming language that lets you work quickly and integrate systems more effectively'
 result_6 = None
+result_6 = string_5[::4]
+print("result_6 " + result_6)
 
 
 # Slice string string_6 from the first to 14th (including 14th) character, save only every third character and save
@@ -37,14 +56,24 @@ result_6 = None
 
 string_6 = 'Python is a programming language that lets you work quickly and integrate systems more effectively'
 result_7 = None
+result_7 = string_6[0:15:3]
+print("result_7 " + result_7)
 
 
 # Save to variable result_8 string value from string_7 variable, written in reverse order, using slicing.
 
 string_7 = 'Python'
 result_8 = None
+result_8 = string_7[::-1]
+print("result_8 " + result_8)
 
 
 # Create a range of numbers from 0 to 10 (excluding 10) and save it to result_9 variable
 
 result_9 = None
+result_9 = range(0,10) #0,1,2,3,4,5,6,7,8,9
+print("result_9 " + str(result_9[0]))
+print("result_9 " + str(result_9[1]))
+print("result_9 " + str(result_9[9]))
+
+
