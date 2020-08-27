71c03bfa5ac3ba9e4b6a5d07694bccf6f045eaba
pecholi <bikmansquad@gmail.com>
2020-08-26 23:29:05 -0500

    hw 5_2 raw

---
+++ lesson_5/homework_5_2.py
@@ -0,0 +1,26 @@
+# WHILE LOOPS EXERCISES
+
+# Enter a random string in the variable string_1, then enter a character and save it in the variable char_1.
+# Write function counter, which will count how many times your character is included in your string
+
+string_1 = None
+char_1 = None
+
+def counter(string, char):
+    pass
+
+
+# Enter a random number and save it in variable number_1. Then create a function number_multiplication
+# that will multiply all the digits together and return the result.
+
+number_1 = None
+def number_multiplication(number):
+    pass
+
+
+# Enter a random number and save it in variable number_2. Then create function number_reverse which will return
+# a number with digits of number_1 in reverse order
+
+number_2 = None
+def number_reverse(number):
+    pass
