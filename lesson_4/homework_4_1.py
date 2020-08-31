fbfb81bbd6402443d9d7f8ab6cbdd2787e4b6b5b
pecholi <bikmansquad@gmail.com>
2020-08-31 18:30:29 -0500

    hw 4_1,4_2

--- lesson_4/homework_4_1.py
+++ lesson_4/homework_4_1.py
@@ -9,41 +9,41 @@ result_2 = string_1[-1]
 # Save to variable result_3 string value from string_2 variable, written in reverse order, using concatenation.
 
 string_2 = 'Python'
-result_3 = None
+result_3 = string_2[5]+ string_2[4]+string_2[3]+string_2[2]+string_2[1]+string_2[0]
 
 
 # Slice string string_3 from 5th to 20th (excluding 20th) character and save the result to variable result_4
 
 string_3 = 'Python is a programming language that lets you work quickly and integrate systems more effectively'
-result_4 = None
+result_4 = string_3[5:20]
 
 
 # Slice string string_4 from 10th character to the end of the string. Save only every second character to variable
 # result_5
 
 string_4 = 'Python is a programming language that lets you work quickly and integrate systems more effectively'
-result_5 = None
+result_5 = string_4[10::2]
 
 
 # Slice string string_5 from the first to the last character, save only every forth character and
 # save the result to variable result_6
 
 string_5 = 'Python is a programming language that lets you work quickly and integrate systems more effectively'
-result_6 = None
+result_6 = string_5[0:-1:4]
 
 
 # Slice string string_6 from the first to 14th character, save only every third character and save
 # the result to variable result_7
 
 string_6 = 'Python is a programming language that lets you work quickly and integrate systems more effectively'
-result_7 = None
+result_7 = string_6[0:14:3]
 
 # Save to variable result_8 string value from string_7 variable, written in reverse order, using slicing.
 
 string_7 = 'Python'
-result_8 = None
+result_8 = string_7[::-1]
 
 
 # Create a range of numbers from 0 to 10 and save it to result_9 variable
 
-result_9 = None
+result_9 = range(10)
--- lesson_4/homework_4_2.py
+++ lesson_4/homework_4_2.py
@@ -2,30 +2,36 @@
 
 # Difference
 # Write a function, which will calculate the difference of these two numbers
+from random import random
+
 
 def difference(num_1, num_2):
-    pass
+    diff = num_1 - num_2
+    return diff
 
 
 # Division
 # Write a function, which will divide these two numbers
 
 def division(num_1, num_2):
-    pass
+    return num_1 / num_2
 
 
 # Function gets random number. If this number is more than ten, return the difference between 100 and this number,
 # otherwise return this number multiplied by 10
 
 def function_1(number):
-    pass
+    number = 3  # random.randint(1, 10)
+    if number > 10:
+        return 100 - number
 
 
 # Your function temerature_convertor gets the temperature in Fahrenheit, convert it to Celsius and return.
 # Formula (32°F − 32) × 5/9 = 0°C
 
 def temerature_convertor(fahrenheit_degree):
-    pass
+    Celsius = (fahrenheit_degree - 32) * 5/9
+    return Celsius
 
 
 # Taxi Fare
@@ -34,7 +40,16 @@ def temerature_convertor(fahrenheit_degree):
 # as its only result rounded by 2 digits. Write a program that demonstrates the function.
 
 def taxi_fare(distance):
-    pass
+    total_fare = round(((distance*1000/140)*0.25)+4, 2)
+    return total_fare
+
+
+# def taxi_fare(distance):
+    # total_fare = float((distance*1000/140)*0.25+4)
+    # result0 = str(total_fare)  # )
+    # result = float(result0[0:6])
+    # return round(result, 2)
+
 
 # examples of usage:
 # taxi_fare(10) #21.86
