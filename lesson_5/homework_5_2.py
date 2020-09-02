e67fd30808c09bc2bfa0cf2d57c0f9bd3df6838d
pecholi <bikmansquad@gmail.com>
2020-09-01 22:52:20 -0500

    hw 5_2

--- lesson_5/homework_5_2.py
+++ lesson_5/homework_5_2.py
@@ -3,24 +3,46 @@
 # Enter a random string in the variable string_1, then enter a character and save it in the variable char_1.
 # Write function counter, which will count how many times your character is included in your string
 
-string_1 = None
-char_1 = None
+string_1 = ('d', 123, 'task', 's')
+char_1 = 's'
+
 
 def counter(string, char):
-    pass
+    count = 0
+    for item in string:
+        if item == char:
+            count += 1
+    return count
 
 
 # Enter a random number and save it in variable number_1. Then create a function number_multiplication
 # that will multiply all the digits together and return the result.
 
-number_1 = None
+number_1 = 123
+
+
 def number_multiplication(number):
-    pass
+    numbers = str(number)
+    length = len(numbers)
+    multiplied: int = 0
+    count = 0
+    while count < length:
+        multiplied *= int(numbers[count])
+        count += 1
+    return multiplied
 
 
 # Enter a random number and save it in variable number_2. Then create function number_reverse which will return
 # a number with digits of number_1 in reverse order
 
-number_2 = None
+number_2 = 4321
+
+
 def number_reverse(number):
-    pass
+    num_str = str(number)
+    digit = len(num_str) - 1
+    res_num = ''
+    while digit >= 0:
+        res_num += num_str[digit]
+        digit -= 1
+    return int(res_num)
