e7c13ddb7ef06a6489734adfb97895abad4fcf25
marmaxkat <marmaxkat@gmail.com>
2020-12-06 21:14:06 -0800

    Inna_Ignatyeva_homework_1_2.py
    Inna Ignatyeva

--- lesson_1/homework_1_2.py
+++ lesson_1/homework_1_2.py
@@ -1,20 +1,21 @@
 # Find the second power of a variable. Save the expression to result_1 variable
 
 a = 10
-result_1 = None
+result_1 = 10**2
+print(result_1)
 
 
 # Convert integer variable b to a float. Save the expression to result_2 variable
 
 b = 10
-result_2 = None
-
+result_2 = float(b)
+print(result_2)
 
 # Convert a float variable c to integer. Save the expression to result_3 variable
 
 c = 5.04
-result_3 = None
-
+result_3 = int(c)
+print(result_3)
 
 # Sum up variables d and e and then multiply the total by f. Convert result to an integer and save the
 # expression to result_4 variable
@@ -22,4 +23,7 @@ result_3 = None
 d = 5.04
 e = 2
 f = 1.2
-result_4 = None
+result_sum = d + e + f
+result_multiply = result_sum * f
+result_4 = int(result_multiply)
+print(result_4)
