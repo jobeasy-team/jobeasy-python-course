4bf537c0202ae482b652c12dc27a00ce3db4880a
marmaxkat <marmaxkat@gmail.com>
2020-12-09 14:34:23 -0800

    Inna_Ignatyeva_homework_1_3.py

--- lesson_1/homework_1_3.py
+++ lesson_1/homework_1_3.py
@@ -1,18 +1,19 @@
 import math
 
-
 # Round a off to three decimal places.
 
 a = 10.04789
-result_1 = None
+result_1 = round(a, 3)
+print(result_1)
 
 
 # Round up result_2 to greater value.
 
 result_2 = 5 / 2 * 6 + 1.25 - 4
+print(math.ceil(result_2))
 
 
 # Round up result_3 to lesser value.
 
 result_3 = 8 / 3 * 5 + 4.75 - 7
-
+print(math.floor(result_3))
