7796eb2208d7ddb030a3876e70539f32563971c8
ashimabmi <ashimabmi@gmail.com>
2020-12-09 12:35:21 -0800

    this is home work 1

--- lesson_1/homework_1_1.py
+++ lesson_1/homework_1_1.py
@@ -1,53 +1,39 @@
 # Sum up a and b. Write expression to the result_sum variable
-
-a = 10
-b = 15
-result_sum = None
-
-
+a: int = 10
+b: int = 15
+result_sum: int = a+b
+print(result_sum)
 # Find the difference between c and d. Save the expression to the result_diff variable
-
-c = 10
-d = 15
-result_diff = None
-
-
+c: int = 10
+d: int = 15
+result_diff: int = a-b
+print(result_diff)
 # Multiply e by f. Save the expression to the result_multiplication variable
-
-e = 10
-f = 15
-result_multiplication = None
-
-
+e: int = 10
+f: int = 15
+result_multiplication: int = e*f
+print(result_multiplication)
 # Divide g by h. Save the expression to the result_division variable
-
-g = 10
-h = 15
-result_division = None
-
-
+e: int = 10
+f: int = 15
+result_division: int = e/f
+print(result_division)
 # Calculate the floored division result of i and j. Save the expression to the result_division_floored variable
-
-i = 10
-j = 15
-result_division_floored = None
-
-
+i: int = 10
+j: int = 15
+result_division_floored: int = i//j
+print(result_division_floored)
 # Calculate the remainder of k divided by l. Save expression to the result_division_remainder variable
-
-k = 10
-l = 15
-result_division_remainder = None
-
-
+k: int = 10
+l: int = 15
+result_division_remainder: int = 10 % 15
+print(result_division_remainder)
 # Rise m to the power n. Save the expression to the result_power variable
-
-m = 5
-n = 2
-result_power = None
-
-
+m: int = 5
+n: int = 2
+result_power: int = pow(m, n)
+print(result_power)
 # Convert an o variable to negative. Save the expression to the result_negative variable
-
-o = 100
-result_negative = None
+o: int = 100
+result_negative: int = (- o)
+print(result_negative)
